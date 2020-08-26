7b6e363721ca234f079c8e5a510aea5c08c09dfa
Niclas Liimatainen <niclas.liimatainen@digia.com>
2020-08-26 14:37:41 +0300

    Update (label bug)

---
+++ precache-manifest.f6918e3d922c45185e370d006b6d1bec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d97665985eb376cacff7dbb4aa54db96",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.5cdc2525393d5e6889f4.bundle.js"
+  },
+  {
+    "url": "runtime~main.5cdc2525393d5e6889f4.bundle.js"
+  },
+  {
+    "revision": "1f56e80c1925a93a11c5",
+    "url": "static/css/main.9554015d.chunk.css"
+  },
+  {
+    "revision": "6b7b3228fb2a8091367a",
+    "url": "static/css/vendors~main.97f6d3c5.chunk.css"
+  },
+  {
+    "revision": "4c70699834d6db6a3fbdb0e27cfca740",
+    "url": "static/media/placeholder_1920x1080.4c706998.jpg"
+  },
+  {
+    "url": "vendors~main.5cdc2525393d5e6889f4.bundle.js"
+  },
+  {
+    "url": "vendors~main.5cdc2525393d5e6889f4.bundle.js.LICENSE.txt"
+  }
+]);
