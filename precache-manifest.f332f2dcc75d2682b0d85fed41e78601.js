639b4a5622a68b889ed9d643fbf0325b476c67c6
Niclas Liimatainen <niclas.liimatainen@digia.com>
2020-09-30 10:25:17 +0300

    Update (select)

---
+++ precache-manifest.f332f2dcc75d2682b0d85fed41e78601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d7aeeaa8827e40f1b9e9227241184d",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.f4cf6df5cce1994cd288.bundle.js"
+  },
+  {
+    "url": "runtime~main.f4cf6df5cce1994cd288.bundle.js"
+  },
+  {
+    "revision": "8d1b034636d0e50fd7e8",
+    "url": "static/css/main.106a879e.chunk.css"
+  },
+  {
+    "revision": "4c70699834d6db6a3fbdb0e27cfca740",
+    "url": "static/media/placeholder_1920x1080.4c706998.jpg"
+  },
+  {
+    "url": "vendors~main.f4cf6df5cce1994cd288.bundle.js"
+  },
+  {
+    "url": "vendors~main.f4cf6df5cce1994cd288.bundle.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.04c392f873abcff8fd5397a3452fffaf.js"
+  "precache-manifest.f332f2dcc75d2682b0d85fed41e78601.js"
 );
 
 self.addEventListener('message', (event) => {
