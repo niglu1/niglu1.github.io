417d51154e68f7f786e5bb001d48b8ee0401a1e7
Niclas Liimatainen <niclas.liimatainen@digia.com>
2020-10-26 11:48:04 +0200

    Update (navigation)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.311b6c1801b5fc7143cc344bddbd8dd4.js"
+  "precache-manifest.4567347b8f163b8e7ce979b853686b0a.js"
 );
 
 self.addEventListener('message', (event) => {
